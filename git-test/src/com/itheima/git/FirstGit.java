8bb9752e2d3c541afe88e7753f5ec0b34aef7255
16060 <1606055548@qq.com>
2021-03-21 22:18:18 +0800

    把本地工程添加到GIT

---
+++ git-test/src/com/itheima/git/FirstGit.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @description:
+ * @author: Lxz
+ * @time: 2021/3/21 21:26
+ */
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("修改了");
+    }
+}
